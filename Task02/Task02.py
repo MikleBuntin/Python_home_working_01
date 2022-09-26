a7ac096ed22b0e817433557ea8582a2cf8b380c8
MikleBuntin <BuntinMA@mail.ru>
2022-09-26 18:54:07 +0400

    Решил задачу 2.

--- Task02/Task02.py
+++ Task02/Task02.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+# Напишите программу для. проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
+# для всех значений предикат.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(f'x = , {x}, y = , {y}, z = , {z} ')
+            result = bool(-(x and y and z) == -x or -y or -z)
+            print('"¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z" is', result, '\n')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
